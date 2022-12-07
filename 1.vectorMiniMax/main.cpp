0469cd4873812934f6c56e9702e9353bf46c6c0e
SkyWaet <SkyWaet@yandex.ru>
2022-12-08 01:02:51 +0300

    intermediate commit

--- 1.vectorMiniMax/main.cpp
+++ 1.vectorMiniMax/main.cpp
@@ -5,31 +5,36 @@
 #include "mpi.h"
 
 int getRandomInteger(int lower, int upper);
-int *initWithRandomValues(const int size);
+void fillWithRandomValues(int *arr, int size);
 int findMinSingleThread(int *array, int from, int to);
 
 int main(int argc, char *argv[])
 {
+    MPI_Status status;
     MPI_Init(&argc, &argv);
+    int size;
+    if (atoi(argv[1]) == 0)
+    {
+        size = 100;
+    }
+    else if (atoi(argv[1]) == 1)
+    {
+        size = 100000;
+    }
+    else
+    {
+        size = 100000000;
+    }
 
-    int size = 10;
+    double startTime = 0;
+    double endTime = 0;
 
     int ProcRank, ProcNum, totalMin;
 
     MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-    int *array;
-    if (ProcRank == 0)
-    {
-        srand(time(0));
-        array = initWithRandomValues(size);
-        totalMin = array[0];
-        int singleThreadRes = findMinSingleThread(array, 0, size);
-        printf("Single thread ver = %d\n", singleThreadRes);
-    }
 
-    MPI_Bcast(array, size, MPI_INT, 0, MPI_COMM_WORLD);
-
+    int *array = (int *)malloc(sizeof(int) * size);
     int k = size / ProcNum;
     int i1 = k * ProcRank;
     int i2 = k * (ProcRank + 1);
@@ -38,24 +43,54 @@ int main(int argc, char *argv[])
     {
         i2 = size;
     }
-    int localMin = findMinSingleThread(array, i1, i2);
-    MPI_Reduce(&localMin, &totalMin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
 
     if (ProcRank == 0)
     {
-        printf("mpi ver = %d\n", totalMin);
+        srand(time(0));
+        fillWithRandomValues(array, size);
+        double stStartTime = MPI_Wtime();
+        int stResult = findMinSingleThread(array, 0, size);
+        double stEndTime = MPI_Wtime();
+        printf("Single thread result: %d, elapsed time: %0.15f, array size %d\n", stResult, stEndTime - stStartTime, size);
+        startTime = MPI_Wtime();
+        for (int i = 1; i < ProcNum; i++)
+        {
+            MPI_Send(array + i1, i2 - i1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
     }
-    MPI_Finalize();
+    else
+    {
+        MPI_Recv(array + i1, i2 - i1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    }
 
+    int localMin = findMinSingleThread(array, i1, i2);
+
+    int reduceResult = MPI_Reduce(&localMin, &totalMin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+
+    if (reduceResult != MPI_SUCCESS)
+    {
+        printf("Rank %d error in reduce: %d\n", ProcRank, reduceResult);
+    }
+
+    if (ProcRank == 0)
+    {
+        endTime = MPI_Wtime();
+        printf("Multi-Thread result: %d, elapsed time: %.15f, number of threads: %d\n", totalMin, endTime - startTime, ProcNum);
+    }
+    free(array);
+    MPI_Finalize();
     return 0;
 }
 
 int findMinSingleThread(int *array, int from, int to)
 {
-    int min = array[0];
-    for (int i = from; i < to; i++)
+    int min = array[from];
+    for (int i = from + 1; i < to; i++)
     {
-        min = std::min(min, array[i]);
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
     }
     return min;
 }
@@ -65,12 +100,10 @@ int getRandomInteger(int lower, int upper)
     return std::rand() % (upper - lower + 1) + lower;
 }
 
-int *initWithRandomValues(const int size)
+void fillWithRandomValues(int *arr, int size)
 {
-    int *arr = new int[size];
     for (int i = 0; i < size; i++)
     {
         arr[i] = getRandomInteger(-100, 100);
     }
-    return arr;
 }
